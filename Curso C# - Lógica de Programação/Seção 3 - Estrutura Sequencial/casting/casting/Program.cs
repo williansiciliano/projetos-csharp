ca22839180b463b76669132789b3f32511a1d251
Willian Siciliano <110183858+williansiciliano@users.noreply.github.com>
2023-04-03 21:24:09 -0300

    area trapezio

---
+++ Curso C# - Lógica de Programação/Seção 3 - Estrutura Sequencial/casting/casting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace casting {
+    class Program { 
+        static void Main(string[] args) {
+
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+
+            resultado = (double) a / b; // quando dividmos um inteiro por outro, o resultado será inteiro, ainda que a variável seja DOUBLE.
+                                        // Então, para converter temos que a fazer um CASTING.
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
+
