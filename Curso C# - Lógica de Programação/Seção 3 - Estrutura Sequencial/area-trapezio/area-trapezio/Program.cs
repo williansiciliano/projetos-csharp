8c8811852fc5626d87cc024587dd701ba400ddef
Willian Siciliano <110183858+williansiciliano@users.noreply.github.com>
2023-04-03 21:21:53 -0300

    Create Program.cs

---
+++ Curso C# - Lógica de Programação/Seção 3 - Estrutura Sequencial/area-trapezio/area-trapezio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace area_trapezio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double b, B, h, area;
+
+            b = 6.0; // quando usarmos DOUBLE, convém escrever o ".0" para indicar que não se trata de um n° inteiro. Se fosse FLOAT usaríamos "6f" no lugar de "6.0"
+            B = 8.0;
+            h = 5.0;
+
+            area = (B + b) / 2.0 * h; // se fosse FLOAT, usaríamos "2f" no lugar de "2.0"
+
+            Console.WriteLine(area);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
